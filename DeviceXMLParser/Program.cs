59259c3161a4ea71d388cba976d0d2bfd659a0d6
bgraefnitz <36892307+bgraefnitz@users.noreply.github.com>
2020-12-22 22:56:48 -0500

    adding actual code

--- DeviceXMLParser/Program.cs
+++ DeviceXMLParser/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System.Xml.Linq;
 
 namespace DeviceXMLParser
 {
@@ -6,7 +7,31 @@ namespace DeviceXMLParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (StreamWriter file = new StreamWriter(@"E:\Downloads\CHKDeviceFiles\facs.csv"))
+            {
+                var directories = Directory.GetDirectories(@"E:\Downloads\CHKDeviceFiles\DeviceFiles");
+                foreach (var xmlFile in Directory.GetFiles(@"E:\Downloads\CHKDeviceFiles\DeviceFiles", "*.xml", SearchOption.AllDirectories))
+                {
+                    try
+                    {
+                        var xml = XDocument.Load(xmlFile);
+                        if (xml.Root.Element("Device").Attribute("category").Value == "RD")
+                        {
+                            var devName = xml.Root.Element("Device").Attribute("device_id").Value;
+
+
+                            var facLinks = xml.Root.Element("Device").Element("FacilityLinks").Elements("FacilityLink");
+                            foreach (var facLink in facLinks)
+                            {
+                                var ordinal = facLink.Attribute("ordinal").Value;
+                                var facId = facLink.Attribute("id").Value;
+                                file.WriteLine(xmlFile + ", " + devName + ", " + ordinal + ", " + facId);
+                            }
+                        }
+                    }
+                    catch { }
+                }
+            }
         }
     }
 }
